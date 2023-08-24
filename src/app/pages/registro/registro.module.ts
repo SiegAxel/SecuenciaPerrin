fa1dbc26a90d92398006d63b65d9283c68748b9b
SiegAxel <f.torresa@hotmail.com>
2023-08-24 19:55:57 -0400

    Revert "v7 Clase Alan"
    This reverts commit 72e2bd86219c76af37842b9a5f1d3bacc578858d.

--- src/app/pages/registro/registro.module.ts
+++ src/app/pages/registro/registro.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FormsModule } from '@angular/forms';
 
 import { IonicModule } from '@ionic/angular';
 
@@ -13,8 +13,7 @@ import { RegistroPage } from './registro.page';
     CommonModule,
     FormsModule,
     IonicModule,
-    RegistroPageRoutingModule,
-    ReactiveFormsModule
+    RegistroPageRoutingModule
   ],
   declarations: [RegistroPage]
 })
